2638beb87bfbc533d9ce3bf6de4d634bb56b1760
ChefDetritus <45082318+ChefDetritus@users.noreply.github.com>
2018-11-21 10:46:36 -0700

    Update main.js
    Adds a function that should allow the player to click a button that increases a number

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-alert("This is an alert box!");
+var varStick=0;
+var varNatureDarmaKa;
+
+alert("...");
+alert("Where am I?...");
+alert("Wh... Who am I?");
+alert("*As you look around, you see lots of vegitation, small critters, and a dense forest filled with bamboo and purple heart trees*");
+alert("*You look down, and a stick on the ground becomes your focus point*");
+alert("*An overwhelming urge compels you collect all the sticks!*");
+
+function increaseStick() {
+  varStick++;
+}
+
+<button onclick=function increaseStick(); {
+    document.write("Stick ="varStick;)
+}>"Stick"</button>
