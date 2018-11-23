5ac0d969eec8ed49541c0a3f66827c19a4dc98a4
ChefDetritus <45082318+ChefDetritus@users.noreply.github.com>
2018-11-22 21:23:25 -0700

    Update main.js
    -Changed Alerts
    -Commented out are reworked onclick function

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 var varStick=0;
 var varNatureDarmaKa;
 
-window.alert("...");
-window.alert("Where am I?...");
-window.alert("Wh... Who am I?");
-window.alert("*As you look around, you see lots of vegitation, small critters, and a dense forest filled with bamboo and purple heart trees*");
-window.alert("*You look down, and a stick on the ground becomes your focus point*");
-window.alert("*An overwhelming urge compels you collect all the sticks!*");
+alert("...");
+alert("Where am I?...");
+alert("Wh... Who am I?");
+alert("*As you look around, you see lots of vegitation, small critters, and a dense forest filled with bamboo and purple heart trees*");
+alert("*You look down, and a stick on the ground becomes your focus point*");
+alert("*An overwhelming urge compels you collect all the sticks!*");
 
 function increaseStick() {
   varStick++;
 }
 
-<button onclick=function increaseStick(); {
+/*
+  typicalbutton.onclick=function increaseStick() {
     document.write("Stick ="varStick;)
-}>"Stick"</button>
+  }
+  
+*/
